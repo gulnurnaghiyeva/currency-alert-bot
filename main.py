9f109392c8bbdd892c79df52d9aafa319229186e
user77777777777777 <gulnurnagiyeva777@gmail.com>
2026-08-05 00:25:54 +0300

    feat: add SMTP email notification function

--- main.py
+++ main.py
@@ -1,25 +1,59 @@
 import requests
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 def get_usd_try_rate():
     
     url = "https://open.er-api.com/v6/latest/USD"
-    
     try:
         response = requests.get(url)
         data = response.json()
         
         if response.status_code == 200 and data.get("result") == "success":
-            usd_try = data["rates"]["TRY"]
-            return usd_try
+            return data["rates"]["TRY"]
         else:
             print("API'den veri çekilirken bir sorun oluştu.")
             return None
-            
     except Exception as e:
         print(f"Bağlantı hatası: {e}")
         return None
 
-if __name__ == "__main__":
-    rate = get_usd_try_rate()
-    if rate:
-        print(f"[TEST] Güncel USD/TRY Kuru: {rate:.2f} TL")
+def send_email_alert(current_rate, target_rate, sender_email, sender_password, receiver_email):
+    
+    
+    # E-posta 
+    subject = f"🚨 KURU UYARISI: Dolar {current_rate:.2f} TL Oldu!"
+    body = f"""
+    Merhaba,
+    
+    Takip ettiğiniz USD/TRY kuru belirlediğiniz hedef seviyeyi aştı!
+    
+    Güncel Kur: {current_rate:.2f} TL
+    Hedef Kurunuz: {target_rate:.2f} TL
+    
+    Bu e-posta Currency Alert Bot tarafından otomatik gönderilmiştir.
+    """
+    
+    # E-posta Nesnesini Oluşturma
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain', 'utf-8'))
+
+    try:
+        # Gmail SMTP Sunucusuna Bağlanma (Port 587)
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls() # Güvenli bağlantı
+        
+        # Girişve E-posta
+        server.login(sender_email, sender_password)
+        server.send_message(msg)
+        server.quit()
+        
+        print(f"✅ Uyarı e-postası başarıyla gönderildi: {receiver_email}")
+        return True
+    except Exception as e:
+        print(f"❌ E-posta gönderilirken hata oluştu: {e}")
+        return False
